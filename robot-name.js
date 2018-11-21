b0c98291b947bc2f0650826cbd6e61954297da2a
Cara Hill <cara@rabidtech.co.nz>
2018-11-21 16:21:45 +1300

    First test passing.

---
+++ robot-name.js
@@ -0,0 +1,23 @@
+class Robot {
+  constructor() {
+    this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    this.numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  }
+
+  get name() {
+    let newName = [];
+
+    for(let i = 0; i < 2; i++) {
+      newName.push(this.alphabet[Math.floor(Math.random()*this.alphabet.length)]);
+    }
+
+    for(let i = 0; i < 3; i++) {
+      newName.push(this.numbers[Math.floor(Math.random()*this.numbers.length)]);
+    }
+
+    return newName.join("");
+  }
+}
+
+export default Robot
