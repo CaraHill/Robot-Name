c9bedb69d39f341ec60465013958bda08c146740
Cara Hill <cara@rabidtech.co.nz>
2018-11-21 17:00:32 +1300

    Second test passing

--- robot-name.js
+++ robot-name.js
@@ -3,6 +3,8 @@ class Robot {
     this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     this.numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    this.robotName;
   }
 
   get name() {
@@ -16,7 +18,9 @@ class Robot {
       newName.push(this.numbers[Math.floor(Math.random()*this.numbers.length)]);
     }
 
-    return newName.join("");
+    this.robotName = this.robotName || newName.join("");
+
+    return this.robotName;
   }
 }
 
