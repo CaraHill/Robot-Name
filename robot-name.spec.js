e8035694a9c351e117bafd7712607e664054ea37
Cara Hill <cara@rabidtech.co.nz>
2018-11-22 16:19:37 +1300

    7 tests passesing, two still commented out (due to not passing)

--- robot-name.spec.js
+++ robot-name.spec.js
@@ -64,7 +64,7 @@ describe('Robot', () => {
   });
 
 
-  xtest('new names should not be sequential', () => {
+  test('new names should not be sequential', () => {
     const name1 = robot.name;
     const name2 = (new Robot()).name;
     const name3 = (new Robot()).name;
@@ -73,7 +73,7 @@ describe('Robot', () => {
     expect(areSequential(name2, name3)).toBe(false);
   });
 
-  xtest('names from reset should not be sequential', () => {
+  test('names from reset should not be sequential', () => {
     const name1 = robot.name;
     robot.reset();
     const name2 = robot.name;
